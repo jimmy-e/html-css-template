c7062155a0e97139cbcbd852b3dde4c63afb87b1
Jimmy <jimmy.e@outlook.com>
2025-03-22 16:37:08 -0500

    Updating - 2025-03-22 16:37

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+const iframe = document.querySelector('#iFrame');
 
+iframe.addEventListener('load', () => {
+  console.log('iframe loaded');
+  let iframeHeight = iframe.contentWindow.document.body.scrollHeight;
+  let iframeWidth = iframe.contentWindow.document.body.scrollWidth;
+  console.log('iframe height', iframeHeight);
+  console.log('iframe width', iframeWidth);
+
+  iframe.style.height = iframeHeight + 'px';
+  iframe.style.width = iframeWidth + 'px';
+
+  document.body.style.paddingBottom = iframeHeight + 20 + 'px';
+  document.body.style.paddingRight = iframeWidth + 20 + 'px';
+});
