633e77519c1f0f5af0578958e51d7c9ac698556a
Jimmy <jimmy.e@outlook.com>
2025-04-02 17:16:09 -0500

    Updating - 2025-04-02 17:16

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const button = document.getElementById('chatbot-button');
+const pawIcon = document.getElementById('paw-icon');
+const xIcon = document.getElementById('x-icon');
 const iFrameContainer = document.getElementById('iFrameContainer');
 
 button.addEventListener('click', function(event) {
   iFrameContainer.style.visibility = 'visible';
+  pawIcon.style.visibility = 'hidden';
+  xIcon.style.visibility = 'visible';
 });
