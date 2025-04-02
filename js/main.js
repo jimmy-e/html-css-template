e28a19dff703ebc196edb96c96fd92b0e38ce4ab
Jimmy <jimmy.e@outlook.com>
2025-04-02 17:21:33 -0500

    Updating - 2025-04-02 17:21

--- js/main.js
+++ js/main.js
@@ -3,8 +3,19 @@ const pawIcon = document.getElementById('paw-icon');
 const xIcon = document.getElementById('x-icon');
 const iFrameContainer = document.getElementById('iFrameContainer');
 
+let isOpen = true;
+
 button.addEventListener('click', function(event) {
-  iFrameContainer.style.visibility = 'visible';
-  pawIcon.style.visibility = 'hidden';
-  xIcon.style.visibility = 'visible';
+
+  if (isOpen) {
+    iFrameContainer.style.visibility = 'visible';
+    pawIcon.style.visibility = 'hidden';
+    xIcon.style.visibility = 'visible';
+  } else {
+    iFrameContainer.style.visibility = 'hidden';
+    pawIcon.style.visibility = 'visible';
+    xIcon.style.visibility = 'hidden';
+  }
+
+  isOpen = !is;
 });
